7291c51b29957a2823ae062cdb52121f6676528d
carlo <carlo.vanderstoep@gmail.com>
2020-05-01 14:31:27 +0200

    GUI labels updated

--- src/listener/GUI.java
+++ src/listener/GUI.java
@@ -5,19 +5,26 @@ import java.awt.*;
 
 public class GUI implements TextListener{
     private JTextField lastTextSend, numTextLines;
+    private JLabel infoLabel, infoLabelLastTextLine;
     private static Integer numLines;
     public GUI(){
 
         JFrame frame = new JFrame("Text produced");
         frame.setSize(400,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
 
         frame.getContentPane().setLayout(new FlowLayout());
         lastTextSend = new JTextField("Last Line of Text",20);
         numLines = 0;
         numTextLines = new JTextField("Number of lines send: " + Integer.toString(numLines) ,20);
+        infoLabel = new JLabel("info label text ");
+        infoLabelLastTextLine = new JLabel("last text line input ");
         frame.getContentPane().add(lastTextSend);
+        frame.getContentPane().add(infoLabelLastTextLine);
         frame.getContentPane().add(numTextLines);
+        frame.getContentPane().add(infoLabel);
 
 
         frame.setVisible(true);
