d220664ba6614cf5fa5c5ae129a3bede7b1451b3
Lex van der Stoep <lex.vanderstoep@gmail.com>
2020-05-01 18:45:37 +0200

    Small changes.

--- src/listener/GUI.java
+++ src/listener/GUI.java
@@ -3,38 +3,34 @@ package listener;
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI implements TextListener{
-    private JTextField lastTextSend, numTextLines;
-    private static Integer numLines;
-    public GUI(){
+public class GUI implements TextListener {
+    private JTextField lastTextSend;
+    private JTextField numTextLines;
+    private Integer numLines;
 
+    public GUI() {
+        initialiseComponents();
+    }
+
+    private void initialiseComponents() {
         JFrame frame = new JFrame("Text produced");
-        frame.setSize(400,200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        lastTextSend = new JTextField("Last Line of Text", 20);
+        numTextLines = new JTextField("Number of lines send: " + numLines, 20);
 
         frame.getContentPane().setLayout(new FlowLayout());
-        lastTextSend = new JTextField("Last Line of Text",20);
-        numLines = 0;
-        numTextLines = new JTextField("Number of lines send: " + Integer.toString(numLines) ,20);
         frame.getContentPane().add(lastTextSend);
         frame.getContentPane().add(numTextLines);
 
-
         frame.setVisible(true);
-    }
-    public static void main(String[] args){
-        GUI gui = new GUI();
     }
 
     @Override
     public void textUpdate(String s) {
-        this.lastTextSend.setText(s);
-        numLines +=1;
-        this.numTextLines.setText("Number of lines send: " + Integer.toString(numLines));
-
+        lastTextSend.setText(s);
+        numLines += 1;
+        numTextLines.setText("Number of lines send: " + numLines);
     }
-
-
-
 }
-
