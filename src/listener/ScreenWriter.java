e6d196126a37e96fab01f828a73e7377537434f2
Lex van der Stoep <lex.vanderstoep@gmail.com>
2020-05-08 16:33:54 +0200

    Set-up Maven

--- src/listener/ScreenWriter.java
+++
@@ -1,9 +0,0 @@
-package listener;
-
-public class ScreenWriter implements TextListener {
-
-    @Override
-    public void textUpdate(String s) {
-        System.out.println("You typed: " + s);
-    }
-}
--- src/listener/TextListener.java
+++
@@ -1,5 +0,0 @@
-package listener;
-
-public interface TextListener {
-    void textUpdate(String s);
-}
