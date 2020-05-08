cdb3a00ca99c150be90b029cfb365d90961c5be7
Lex van der Stoep <lex.vanderstoep@gmail.com>
2020-05-08 16:40:16 +0200

    Set-up Maven

---
+++ src/main/java/com/carlo/listener/DiscFileWriter.java
@@ -0,0 +1,34 @@
+package com.carlo.listener;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DiscFileWriter implements TextListener {
+    private static final Logger LOGGER = Logger.getLogger(DiscFileWriter.class.getName());
+    private File file;
+
+    public DiscFileWriter(String logFile) {
+        file = new File(logFile);
+
+        try {
+            if (file.createNewFile()){
+                LOGGER.log(Level.INFO, "A new file has been created: " + logFile);
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while creating a new file.", e);
+        }
+    }
+
+    @Override
+    public void textUpdate(String s)  {
+        try (FileWriter writer = new FileWriter(file,true)){
+            writer.write(s+ "\n");
+            writer.flush();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "An error occurred while trying to write to the file: " + file.getAbsolutePath(), e);
+        }
+    }
+}
---
+++ src/test/java/com/carlo/listener/ExampleTest.java
@@ -0,0 +1,21 @@
+package com.carlo.listener;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Example test for Listener.
+ */
+public class ExampleTest {
+    @Test
+    public void happyTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void bigEvilTest()
+    {
+        assertTrue( false );
+    }
+}
